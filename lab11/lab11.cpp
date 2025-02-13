41472b249934997a0e701ff661766fb88b7ffe37
Daniil Zdanovskii <114250637+slarmick@users.noreply.github.com>
2025-02-13 12:17:01 +0200

    Add files via upload

---
+++ lab11/lab11.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Функция для разбиения массива
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high]; // Выбираем последний элемент как опорный
+    int i = (low - 1); // Индекс элемента, меньшего, чем опорный
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]); // Меняем местами опорный элемент с элементом на i + 1
+    return (i + 1); // Возвращаем позицию опорного элемента
+}
+
+// Рекурсивная функция для быстрой сортировки
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        // Разбиваем массив на две части
+        int pi = partition(arr, low, high);
+
+        // Рекурсивно сортируем левую и правую части
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+
+    vector<int> arr;
+    int num;
+
+    cout << "Введите числа (введите -1 для завершения ввода): ";
+
+    // Ввод чисел с клавиатуры
+    cin >> num;
+    while (num != -1) {
+        arr.push_back(num);
+        cin >> num;
+    }
+
+    cout << "Исходная последовательность: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    quickSort(arr, 0, arr.size() - 1);
+
+    cout << "Отсортированная последовательность: ";
+    for (int num : arr) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ lab12/lab12.cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// Функция для слияния двух отсортированных векторов
+vector<int> merge(const vector<int>& left, const vector<int>& right) {
+    vector<int> result;
+    size_t i = 0, j = 0;
+
+    // Слияние двух векторов
+    while (i < left.size() && j < right.size()) {
+        if (left[i] < right[j]) {
+            result.push_back(left[i]);
+            i++;
+        }
+        else {
+            result.push_back(right[j]);
+            j++;
+        }
+    }
+
+    // Добавляем оставшиеся элементы из левого вектора
+    while (i < left.size()) {
+        result.push_back(left[i]);
+        i++;
+    }
+
+    // Добавляем оставшиеся элементы из правого вектора
+    while (j < right.size()) {
+        result.push_back(right[j]);
+        j++;
+    }
+
+    return result;
+}
+
+// Функция для многофазовой сортировки
+vector<int> multiwayMergeSort(const vector<int>& arr) {
+    if (arr.size() <= 1) {
+        return arr;
+    }
+
+    // Разделяем массив на две части
+    size_t mid = arr.size() / 2;
+    vector<int> left(arr.begin(), arr.begin() + mid);
+    vector<int> right(arr.begin() + mid, arr.end());
+
+    // Рекурсивно сортируем каждую часть
+    left = multiwayMergeSort(left);
+    right = multiwayMergeSort(right);
+
+    // Сливаем отсортированные части
+    return merge(left, right);
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+    vector<int> numbers;
+    int num;
+
+    cout << "Введите последовательность чисел (для завершения введите любой нечисловой символ):" << endl;
+
+    // Чтение чисел с клавиатуры
+    while (cin >> num) {
+        numbers.push_back(num);
+    }
+
+    // Сортировка последовательности
+    vector<int> sortedNumbers = multiwayMergeSort(numbers);
+
+    // Вывод отсортированной последовательности
+    cout << "Отсортированная последовательность:" << endl;
+    for (int n : sortedNumbers) {
+        cout << n << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
